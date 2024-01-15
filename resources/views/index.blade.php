8547882560b26e44c70aca15aa3620503a1eebfa
Talha <mrtalhim@gmail.com>
2024-01-15 21:37:44 +0700

    form ui

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,5 +8,26 @@
 </head>
 <body>
     <h1>Hello</h1>
+    <form action="submit" style="display: flex; flex-direction: column">
+        <ul>
+            <li>Non-Musi</li>
+            <li>Musi-Musiah</li>
+        </ul>
+        <select>
+            <option>Wasiyyat 1/3</option>
+            <option>Wasiyyat 1/5</option>
+            <option>Wasiyyat 1/10</option>
+        </select>
+        <label for="penghasilan">Jumlah Penghasilan Satu Tahun</label>
+        <input id="penghasilan" type="number" placeholder="Masukkan Jumlah Penghasilan">
+        <label for="aam">Candah Aam</label>
+        <input id="aam" type="number" disabled>
+        <label for="jalsah">Jalsah</label>
+        <input id="jalsah" type="number" disabled>
+        <label for="badan">Dana Badan</label>
+        <input id="badan" type="number" disabled>
+        <label for="ijtima">Ijtima Badan</label>
+        <input id="ijtima" type="number" disabled>
+    </form>
 </body>
 </html>
