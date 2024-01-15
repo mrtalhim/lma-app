9bacc8ef60bde9e1be41b9d61a4dcdc4cad576d0
Talha <mrtalhim@gmail.com>
2024-01-15 21:26:50 +0700

    first commit

---
+++ app/Http/Controllers/LmaController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LmaController extends Controller
+{
+    public function show() {
+        return view('index');
+    }
+}
